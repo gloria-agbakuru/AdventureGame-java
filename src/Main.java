bd5ef246fc75b1b6eb8229fe4f5ae1aecb88f19f
Gloria Agbakuru <gagbakuru@sustainaccount.com>
2023-09-01 22:09:48 +0200

    final

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Dark Forest !");
+        System.out.println("Welcome to the Dark Forest !!");
         System.out.println("Try to survive in 10 steps by Jumping (j), Ducking (d), or Moving forward (m) to not be injured by the forest animals");
-        System.out.println("You can only be wounded by the forest animals 3 times before you die, so be careful");
-        System.out.println("You now have 3 lives");
+        System.out.println("You can only be wounded by the forest animals 5 times before you die, so be careful");
+        System.out.println("You now have 5 lives");
+        System.out.println("--------------------------------------------------------------------");
+
 
 
         List<String> itemList1 = new ArrayList<>();
@@ -20,7 +22,7 @@ public class Main {
         itemList1.add("");
         itemList1.add("");
         itemList1.add("");
-
+        itemList1.add("");
 
 
         List<String> itemList2 = new ArrayList<>();
@@ -28,6 +30,7 @@ public class Main {
         itemList2.add("came face to face with a lion");
         itemList2.add("a hundred ants instantly crawled up on you");
         itemList2.add("a badger attacked your face");
+        itemList2.add("");
         itemList2.add("");
         itemList2.add("");
         itemList2.add("");
@@ -43,18 +46,15 @@ public class Main {
         itemList3.add("");
         itemList3.add("");
         itemList3.add("");
-
-
+        itemList3.add("");
 
         Random random = new Random();
         Scanner scanner = new Scanner(System.in);
 
-        int totalIterations = 10;
-        int lives = 3;
+        int totalIterations = 0;
+        int lives = 5;
 
-
-
-        for (int iteration = 1; iteration <= totalIterations; iteration++) {
+        for (int steps = 9; steps >= totalIterations; steps--) {
             int randomIndex1 = random.nextInt(itemList1.size());
             int randomIndex2 = random.nextInt(itemList2.size());
             int randomIndex3 = random.nextInt(itemList3.size());
@@ -65,51 +65,113 @@ public class Main {
 
             System.out.print("Type 'j' 'd' or 'm' to perform an action: ");
             String userInput = scanner.nextLine();
-            if (!randomItem1.isBlank()) {
-                if (userInput.equalsIgnoreCase("J")) {
+            if (userInput.equalsIgnoreCase("j")) {
+                if (!randomItem1.equalsIgnoreCase("")) {
                     System.out.println("You jumped and " + randomItem1);
                     lives--;
+                    if (steps == 1) {
+                        System.out.println("You have " + steps + " step left to get out");
+                    } else {
+                        System.out.println("You have " + steps + " steps left to get out");
+                    }
                     if (lives == 1) {
                         System.out.println("You now have " + lives + " life");
                     } else {
                         System.out.println("You now have " + lives + " lives");
                     }
+                    System.out.println("--------------------------------------------------------------------");
+                }
+                else {
+                    System.out.println("You moved without being injured");
+                    if (steps == 1) {
+                        System.out.println("You have " + steps + " step left to get out");
+                    } else {
+                        System.out.println("You have " + steps + " steps left to get out");
+                    }
+                    if (lives == 1) {
+                        System.out.println("You now have " + lives + " life");
+                    } else {
+                        System.out.println("You now have " + lives + " lives");
+                    }
+                    System.out.println("--------------------------------------------------------------------");
+
                 }
             }
-            if (!randomItem2.isBlank()) {
-                if (userInput.equalsIgnoreCase("D")) {
+            if (userInput.equalsIgnoreCase("d")) {
+                if (!randomItem2.equalsIgnoreCase("")) {
                     System.out.println("You ducked and " + randomItem2);
                     lives--;
+                    if (steps == 1) {
+                        System.out.println("You have " + steps + " step left to get out");
+                    } else {
+                        System.out.println("You have " + steps + " steps left to get out");
+                    }
                     if (lives == 1) {
                         System.out.println("You now have " + lives + " life");
                     } else {
                         System.out.println("You now have " + lives + " lives");
                     }
+                    System.out.println("--------------------------------------------------------------------");
+                }
+                else  {
+                    System.out.println("You moved without being injured");
+                    if (steps == 1) {
+                        System.out.println("You have " + steps + " step left to get out");
+                    } else {
+                        System.out.println("You have " + steps + " steps left to get out");
+                    }
+                    if (lives == 1) {
+                        System.out.println("You now have " + lives + " life");
+                    } else {
+                        System.out.println("You now have " + lives + " lives");
+                    }
+                    System.out.println("--------------------------------------------------------------------");
                 }
             }
-            if (!randomItem3.isBlank()) {
-                if (userInput.equalsIgnoreCase("M")) {
-                    System.out.println("You moved forward and " + randomItem1);
+            if (userInput.equalsIgnoreCase("m")) {
+                if (!randomItem3.equalsIgnoreCase("")) {
+                    System.out.println("You moved forward and " + randomItem3);
                     lives--;
+                    if (steps == 1) {
+                        System.out.println("You have " + steps + " step left to get out");
+                    } else {
+                        System.out.println("You have " + steps + " steps left to get out");
+                    }
                     if (lives == 1) {
                         System.out.println("You now have " + lives + " life");
                     } else {
                         System.out.println("You now have " + lives + " lives");
                     }
+                    System.out.println("--------------------------------------------------------------------");
+                }
+                else {
+                    System.out.println("You moved without being injured");
+                    if (steps == 1) {
+                        System.out.println("You have " + steps + " step left to get out");
+                    } else {
+                        System.out.println("You have " + steps + " steps left to get out");
+                    }
+                    if (lives == 1) {
+                        System.out.println("You now have " + lives + " life");
+                    } else {
+                        System.out.println("You now have " + lives + " lives");
+                    }
+                    System.out.println("--------------------------------------------------------------------");
                 }
             }
-            else {
-                System.out.println("You moved without being injured");
-                if (lives == 1) {
-                    System.out.println("You now have " + lives + " life");
-                } else {
-                    System.out.println("You now have " + lives + " lives");
-                }
+            else if (!userInput.equalsIgnoreCase("j") && !userInput.equalsIgnoreCase("d") && !userInput.equalsIgnoreCase("m")){
+                System.out.println("You have to type either 'j', 'd' or 'm'");
+                System.out.println("--------------------------------------------------------------------");
             }
-
 
             if (lives == 0) {
+                System.out.println("You have 0 lives and have died from injury infections");
                 System.out.println("GAMEOVER");
+                break;
+            }
+
+            if (steps==totalIterations) {
+                System.out.println("YOU GOT OUT OF THE FOREST SAFELY!! YAYYY!!!");
                 break;
             }
         }
